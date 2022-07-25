9e75594bf7c26e0ab030a0a8b179b93775a540b5
RichieWilli <richardwilliam1994@gmail.com>
2022-07-25 13:00:02 +0530

    first

---
+++ src/test/java/org/Practice/Mavenpractice2Test.java
@@ -0,0 +1,35 @@
+package org.Practice;
+
+import org.testng.annotations.Test;
+
+public class Mavenpractice2Test {
+@Test
+
+	public void  mvnsample()
+	{
+		System.out.println("manual");
+		System.out.println("liiii");
+		
+		
+	}
+	@Test
+	public void  mvnsample1() 
+	{
+		System.out.println(" automation");
+		
+	}
+	@Test
+	public void  mvnsample2() 
+	{
+		System.out.println(" codrin");
+		
+	}
+	@Test
+	public void  mvnsample3() 
+	{
+		System.out.println(" i am a  good youtuber");
+		
+	}
+}
+
+
---
+++ src/test/java/org/Practice/MavenpracticeTest.java
@@ -0,0 +1,31 @@
+package org.Practice;
+
+import org.testng.annotations.Test;
+
+public class MavenpracticeTest 
+{
+	@Test
+	public void  mvnsample()
+	{
+		System.out.println(" i am a  good tester");
+		
+	}
+	@Test
+	public void  mvnsample1() 
+	{
+		System.out.println(" i am a  good developer");
+		
+	}
+	@Test
+	public void  mvnsample2() 
+	{
+		System.out.println(" i am a  good coder");
+		
+	}
+	@Test
+	public void  mvnsample3() 
+	{
+		System.out.println(" i am a  good youtuber");
+		
+	}
+}
